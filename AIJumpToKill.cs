4dd7a405b75b13a611ef7c7cc4562f39d5f1d3a0
cgf_dev <charliefrost50@googlemail.com>
2020-07-20 16:03:27 +0100

    AI Scripts Added

---
+++ AIJumpToKill.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIJumpToKill : MonoBehaviour
+{
+    // Variables
+    public int enemyDamage = 1;
+    public int enemyHitRange = 1;
+
+    public GameObject plimpBounce;
+
+    void Start()
+        {
+            InvokeRepeating("EnemyAttack", 2.0f, 3.0f);
+        }
+
+
+
+    public void OnTriggerEnter(Collider withAI)
+        {
+            if (withAI.gameObject.tag == "Player")
+            {
+            // The following 4 lines of code enable the death animation to play for 
+            // 2 seconds without damaging the player
+            foreach (Transform child in transform)
+                child.gameObject.SetActive(false);
+
+            Destroy(transform.gameObject, 2.0f);
+
+            Vector3 Bounce = new Vector3(0, 200, 0);
+            plimpBounce.GetComponent<Rigidbody>().AddForce(Bounce);
+
+
+        }
+
+    }
+
+
+    void EnemyDie()
+    {
+        Debug.Log("Enemy Died");
+    }
+
+    void EnemyAttack()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 origin = transform.position;
+        
+        
+
+        if (Physics.Raycast(origin, forward, out hit, enemyHitRange))
+        {
+            if (hit.transform.gameObject.tag == "Player")
+            {
+                hit.transform.gameObject.SendMessage("PlayerTakesDamage", 1);
+
+            }
+        }
+    }
+}
---
+++ AIPunchToKill.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPunchToKill : MonoBehaviour
+{
+    // Variables
+    public int enemyMaxHealth = 4;
+    public int enemyCurrentHealth;
+    public int enemyDamage = 1;
+    public int enemyHitRange = 1;
+
+
+    // Allow the player to start with full health
+    void Awake()
+    {
+        enemyCurrentHealth = enemyMaxHealth;
+    }
+
+    void Start()
+    {
+        InvokeRepeating("EnemyAttack", 2.0f, 3.0f);
+    }
+
+    public void EnemyTakesDamage(int damage)
+    {
+        enemyCurrentHealth -= damage;
+        Debug.Log("Enemy took damage");
+
+        if (enemyCurrentHealth <= 0)
+            EnemyDie();
+    }
+
+
+    void EnemyDie()
+    {
+        Debug.Log("Enemy Died");
+
+        // Render AI harmless until death anim has been played
+        foreach (Transform GameObject in transform) 
+        gameObject.SetActive(false);
+
+        Destroy(transform.gameObject, 2.0f);
+    }
+
+    void EnemyAttack()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 origin = transform.position;
+
+        if (Physics.Raycast(origin, forward, out hit, enemyHitRange))
+        {
+            if (hit.transform.gameObject.tag == "Player")
+            {
+                hit.transform.gameObject.SendMessage("PlayerTakesDamage", 1);
+            }
+        }
+    }
+
+
+
+}
